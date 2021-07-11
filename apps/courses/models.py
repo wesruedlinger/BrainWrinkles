a85913e9a0b3508b9fe4e29d1e0500c83eb28fd9
Wise Weasel <weasel@Wises-MacBook-Pro.local>
2021-07-10 22:21:48 -0400

    General Update

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -8,13 +8,13 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    
+
     class Meta:
         ordering = ['title']
-        
+
     def __str__(self):
         return self.title
-    
+
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
@@ -23,13 +23,13 @@ class Course(models.Model):
     subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
-        ordering = ['-created']
-        
+        ordering = ['-created'] # order the courses with newest first
+
     def __str__(self):
         return self.title
-    
+
 
 class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
@@ -42,10 +42,34 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in':('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
-    
 
+
+# An abstract model for the content
 class ItemBase(models.Model):
-    
+    title = models.CharField(max_length=250)
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Text(ItemBase): # Part of the ItemBase abstract model
+    content = models.TextField()
+
+class File(ItemBase): # Part of the ItemBase abstract model
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase): # Part of the ItemBase abstract model
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase): # Part of the ItemBase abstract model
+    url = models.URLField()
+
+
