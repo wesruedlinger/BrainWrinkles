a786984fd99b3458ad01b9f385e27e68628ec449
Wise Weasel <weasel@Wises-MacBook-Pro.local>
2021-07-10 21:12:37 -0400

    Inital Commit

---
+++ apps/courses/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.db import models
+from .models import Subject, Course, Module
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+    
+
+class ModuleInLine(admin.StackedInline):
+    model = Module
+    
+    
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInLine]
