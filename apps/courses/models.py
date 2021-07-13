29cd455f816a4acc7703adb47eb2716b8563ce67
Wise Weasel <weasel@Wises-MacBook-Pro.local>
2021-07-13 19:14:24 -0400

    General Update

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.fields import SlugField
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -35,9 +36,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+    
+    class Meta:
+        ordering = ['order']
     
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -45,7 +50,10 @@ class Content(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in':('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
 
+    class Meta:
+        ordering = ['order']
 
 # An abstract model for the content
 class ItemBase(models.Model):
