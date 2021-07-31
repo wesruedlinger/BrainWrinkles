3462e63623c07adde5af8d9402219bb19f7eeb0c
Wise Weasel <weasel@Wises-MacBook-Pro.local>
2021-07-31 15:20:18 -0400

    General Update

--- apps/students/forms.py
+++ apps/students/forms.py
@@ -1,16 +1,7 @@
-from django.views.generic.edit import FormView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CourseEnrollForm
+from django import forms
+from apps.courses.models import Course
 
 
-class StudentEnrollCourseView(LoginRequiredMixin, FormView):
-    course = None
-    form_class = CourseEnrollForm
+class CourseEnrollForm(forms.Form):
+    course = forms.ModelChoiceField(queryset=Course.objects.all(), widget=forms.HiddenInput)
 
-    def form_valid(self, form):
-        self.course = form.cleaned_data['course']
-        self.course.students.add(self.request.user)
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('student_course_detail', args=[self.course.id])
